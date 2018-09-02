31f3e15b92a2624396f949817879fe3905743d11
Diego Andrade <dcandrade19@outlook.com>
2018-09-02 11:39:13 -0300

    teste db

--- src/config.js
+++ src/config.js
@@ -1,10 +1,10 @@
 global.SALT_KEY = 'jku48dh8-jh8h-3jd8-jdh6-j3h874h9k32h';
 
 module.exports = {
-    host: 'localhost',
+    host: 'sql10.freemysqlhosting.net',
     port: 3306,
-    user: 'root',
-    password: '',
-    database: 'dbestagio',
+    user: 'sql10254824',
+    password: 'wMARwuelrX',
+    database: 'sql10254824',
     dialect: 'mysql'
 }
