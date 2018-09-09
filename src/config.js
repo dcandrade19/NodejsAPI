003691ec74828e1ad6809ce15fd63b8fd8ed0982
Diego Andrade <dcandrade19@outlook.com>
2018-09-09 14:46:21 -0300

    db novo

--- src/config.js
+++ src/config.js
@@ -1,10 +1,10 @@
 global.SALT_KEY = 'jku48dh8-jh8h-3jd8-jdh6-j3h874h9k32h';
 
 module.exports = {
-    host: 'sql10.freemysqlhosting.net',
+    host: 'db4free.net',
     port: 3306,
-    user: 'sql10254824',
+    user: 'admin33',
     password: 'wMARwuelrX',
-    database: 'sql10254824',
+    database: 'dbtestes33',
     dialect: 'mysql'
 }
